7b7c984ba02cb86b3f3cfa0008f29ec74375cabd
utkarshgupta123 <107859135+utkarshgupta123@users.noreply.github.com>
2023-01-05 12:19:35 +0530

    Update defaultdata.js

--- defaultdata.js
+++ defaultdata.js
@@ -3,7 +3,7 @@ const productdata = require("./constant/productsdata");
 
 const DefaultData = async()=>{
     try {
-        await Products.deleteMany({});
+//         await Products.deleteMany({});
         const storeData = await Products.insertMany(productdata);
         console.log("from defaultdata.js line 8",storeData);
     } catch (error) {
